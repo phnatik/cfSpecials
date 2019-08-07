639025980c7d84c597489ba692999cbeb8daf1bc
Andrew Price <35412471+phnatik@users.noreply.github.com>
2019-08-07 13:00:58 -0500

    Initial Commit
    Base files for .NET Core base MVC project. This is also a template for reading from and writing to a JSON file.

---
+++ cfSpecials/Controllers/SpecialsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cfSpecials.Models;
+using Newtonsoft.Json;
+
+
+namespace cfSpecials.Controllers
+{
+    public class SpecialsController : Controller
+    {
+        public IActionResult Index()
+        {
+            // need to read the data from file and json that mofo
+            string path = @"C:\Users\andru\source\repos\cfSpecials\cfSpecials\wwwroot\js\current-specials.json";
+
+            //string data = System.IO.File.ReadAllText(path, Encoding.UTF8);
+
+            var str = System.IO.File.ReadAllText(path);
+            var obj = JsonConvert.DeserializeObject<List<Specials>>(str);
+
+
+            return View(obj);
+        }
+
+        // Modifiers(private/public) ReturnType Methodname ArgList
+        //public IActionResult SetData(Specials obj)
+        //{
+            
+        //}
+    }
+}
---
+++ cfSpecials/Models/Specials.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace cfSpecials.Models
+{
+    public class Specials
+    {
+        public string SpecialID { get; set; }
+        public string vTitle { get; set; }
+        public string Condition { get; set; }
+        public string MSRP { get; set; }
+        public string FinalPrice { get; set; }
+        public string TotalSavings { get; set; }
+        public string StockNumber { get; set; }
+        public string VIN { get; set; }
+        public string OrderNumber { get; set; }
+        
+               
+    }
+    
+    
+}
